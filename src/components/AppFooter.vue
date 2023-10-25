3c69ca8581253830c21a728c074879051b1511a9
lawrencedulian <lawrenceedulian@gmail.com>
2023-10-25 11:47:43 +0200

    Add: Header and Footer components

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,75 @@
+<script>
+import { store } from '../store';
+
+export default {
+    name: 'AppFooter',
+    data() {
+        return {
+            store
+        }
+    },
+    computed: {
+        routeBgColor() {
+            const currentRoute = this.$route;
+            return currentRoute.meta.bgColor || '#fffeee'; // Colore di default se non specificato
+        }
+    }
+}
+</script>
+
+<template>
+    <footer :style="{ backgroundColor: routeBgColor }">
+        <section class="container socials rounded">
+            <div class="row">
+                <div class="socials-icon col-4 col-xl-4 col-lg-5 col-md-7 d-flex justify-content-between ms-3">
+                    <a :href="social.link" v-for="(social, index) in store.social" :key="index">
+                        <i :class="social.icon"></i>
+                    </a>
+                    <span class="text-white pe-none d-none d-md-block">lawrenceedulian@gmail.com</span>
+                </div>
+            </div>
+        </section>
+
+        <section class="container copyright p-0 d-flex align-items-center">
+            <p class="mb-0 ps-2 pe-none">COPYRIGHT 2023</p>
+        </section>
+    </footer>
+</template>
+
+<style lang="scss" scoped>
+footer {
+    height: var(--footer-height);
+}
+
+.socials {
+    height: var(--socials-height);
+    background-color: var(--text-color);
+
+    .socials-icon {
+        line-height: var(--socials-height);
+    }
+
+    a {
+        color: white;
+        font-size: 1.8rem;
+    }
+}
+
+.copyright {
+    height: var(--copyright-height);
+
+    p {
+        font-size: .8rem;
+    }
+}
+
+@media screen and (max-width: 575px) {
+    .socials {
+        width: 90%;
+    }
+
+    .copyright {
+        justify-content: center;
+    }
+}
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,51 @@
+<script>
+export default {
+    name: 'AppHeader',
+    props: {
+        isMenuOpen: Boolean, // Pass isMenuOpen as a prop from the parent component
+    },
+    methods: {
+        toggleMenu() {
+            this.$emit('toggleMenu'); // Emit the event to toggle the menu
+        },
+    },
+    computed: {
+        routeBgColor() {
+            const currentRoute = this.$route;
+            return currentRoute.meta.bgColor || '#fffeee'; // Colore di default se non specificato
+        }
+    },
+};
+</script>
+
+<template>
+    <header class="d-flex align-items-center" :style="{ backgroundColor: routeBgColor }">
+        <div class="container d-flex justify-content-end">
+            <div class="hamburger d-flex flex-column justify-content-between" @click="toggleMenu">
+                <div class="bar"></div>
+                <div class="bar"></div>
+                <div class="bar"></div>
+            </div>
+        </div>
+    </header>
+</template>
+  
+
+<style lang="scss">
+header {
+    height: var(--header-height);
+    font-size: var(--font-size-header);
+
+    .hamburger {
+        height: 15px;
+        cursor: pointer;
+
+        .bar {
+            width: 23px;
+            height: 2px;
+            background-color: var(--button-color);
+        }
+    }
+}
+</style>
+    
