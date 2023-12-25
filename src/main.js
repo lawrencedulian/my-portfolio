be5e19580498f2ca62b43495ae74592bc91d05fa
lawrencedulian <lawrenceedulian@gmail.com>
2023-12-25 15:48:19 +0100

    add: fontawesome icons

--- src/main.js
+++ src/main.js
@@ -4,5 +4,7 @@ import { router } from './router'
 import './styles/general.scss'
 
 import '@fontsource-variable/space-grotesk';
+import '@fortawesome/fontawesome-free/css/all.css';
+
 
 createApp(App).use(router).mount('#app')
